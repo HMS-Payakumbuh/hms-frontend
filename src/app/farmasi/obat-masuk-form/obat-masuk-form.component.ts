32fb7743ca4f13c5a6ce46cd0e43268f4a4314ac
Hans Gunawan <Hans Gunawan>
2017-06-16 23:50:45 +0700

    Added daftar obat and jenis obat, moved to farmasi folder for hierarchy, integrated to app

---
+++ src/app/farmasi/obat-masuk-form/obat-masuk-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'obat-masuk-form-page',
+  templateUrl: './obat-masuk-form.component.html'
+})
+
+export class ObatMasukFormComponent {}
