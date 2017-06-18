968ad1e38f3c68714822555443faa42e4b942d92
Juan Anton <juan.anton.che@gmail.com>
2017-06-18 13:47:39 +0700

    now filter using substring & added filter by tanggal

--- src/app/pipe/filter-nama-pasien.pipe.ts
+++ src/app/pipe/filter-nama-pasien.pipe.ts
@@ -3,11 +3,12 @@ import { Pipe, PipeTransform }	from '@angular/core';
 @Pipe({
 	name: 'filterNamaPasien'
 })
+
 export class FilterNamaPasienPipe implements PipeTransform {
   transform(items: Array<any>, param: string): Array<any> {
   	if (!items || !param) {
   		return items;
   	}
-    return items.filter(item => item.nama_pasien.toLowerCase() === param.toLowerCase());
+    return items.filter(item => item.nama_pasien.toLowerCase().indexOf(param.toLowerCase()) > -1);
   }
 }
