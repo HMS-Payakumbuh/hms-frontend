415d2d0b87dea0a07b9047705f94395bfc233477
Calvin <calvin.aditya95@gmail.com>
2017-06-18 22:38:02 +0700

    Created tindakan class

---
+++ src/app/layanan/tindakan.ts
@@ -0,0 +1,12 @@
+export class Tindakan {
+	no_transaksi: number;
+	no_tindakan: number;
+	harga: number;
+	dokumen_penunjang: number;
+	keterangan: string;
+	id_pembayaran: number;
+	kode_tindakan: string;
+	tanggal_waktu: string;
+	id_tenaga_medis: number;
+	nama_layanan: string;
+}
