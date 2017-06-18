e2d93b5e6101ce5c705646edd16d903ea1fa5886
unknown <hcguyz@gmail.com>
2017-06-18 22:28:07 +0700

    Removed unused file from daftar obat rusak

--- src/app/farmasi/daftar-obat-rusak/obat-rusak.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ObatRusak = (function () {
-    function ObatRusak() {
-    }
-    return ObatRusak;
-}());
-exports.ObatRusak = ObatRusak;
-//# sourceMappingURL=obat-rusak.js.map
