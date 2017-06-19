f2e152d96b96673ce3f9d752066e1d8f4242eeb4
Calvin <calvin.aditya95@gmail.com>
2017-06-19 17:14:57 +0700

    Removed all files in .gitignore #2

--- src/app/antrian/antrian.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Antrian = (function () {
-    function Antrian() {
-    }
-    return Antrian;
-}());
-exports.Antrian = Antrian;
-//# sourceMappingURL=antrian.js.map
--- src/app/layanan/diagnosis-reference.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var DiagnosisReference = (function () {
-    function DiagnosisReference() {
-    }
-    return DiagnosisReference;
-}());
-exports.DiagnosisReference = DiagnosisReference;
-//# sourceMappingURL=diagnosis-reference.js.map
