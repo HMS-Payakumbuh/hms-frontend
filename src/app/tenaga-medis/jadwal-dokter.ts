24ef8cd4032eb68c85409f47eeeb8f6840a4985e
Calvin <calvin.aditya95@gmail.com>
2017-06-21 22:38:11 +0700

    Added jadwal dokter list

---
+++ src/app/tenaga-medis/jadwal-dokter.ts
@@ -0,0 +1,8 @@
+export class JadwalDokter {
+	nama_poliklinik: string;
+	id_dokter: number;
+	nama_dokter: string;
+	tanggal: string;
+	waktu_mulai_praktik: string;
+	waktu_selesai_praktik: string;
+}
--- src/app/tenaga-medis/tenaga-medis.service.ts
+++ src/app/tenaga-medis/tenaga-medis.service.ts
@@ -17,6 +17,12 @@ export class TenagaMedisService {
 		{id: 3, nama: 'Jonathan', jabatan: 'Petugas Lab'}
 	];
 
+	allJadwalDokter: JadwalDokter[] = [
+		{nama_poliklinik: 'Poli Umum', id_dokter: 1, nama_dokter: 'Calvin', tanggal:'22/06/2017', waktu_mulai_praktik:'09:00', waktu_selesai_praktik:'15:00'},
+		{nama_poliklinik: 'Poli Umum', id_dokter: 1, nama_dokter: 'Calvin', tanggal:'23/06/2017', waktu_mulai_praktik:'09:00', waktu_selesai_praktik:'15:00'},
+		{nama_poliklinik: 'Poli THT', id_dokter: 2, nama_dokter: 'Aditya', tanggal:'22/06/2017', waktu_mulai_praktik:'09:00', waktu_selesai_praktik:'13:00'}
+	];
+
 	constructor(private http:Http) { }
 
 	private handleError(error: any): Promise<any> {
@@ -32,6 +38,11 @@ export class TenagaMedisService {
 	getTenagaMedis(id: number): Promise<TenagaMedis> {
 		return this.getAllTenagaMedis()
 			.then(allTenagaMedis => allTenagaMedis.find(tenagaMedis => tenagaMedis.id === id))
+			.catch(this.handleError);
+	}
+
+	getAllJadwalDokter(): Promise<JadwalDokter[]> {
+		return Promise.resolve(this.allJadwalDokter)
 			.catch(this.handleError);
 	}
 }
