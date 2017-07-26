8364a076427567d1ae8baeb788c8fc5d7624ece7
Calvin <calvin.aditya95@gmail.com>
2017-07-26 15:26:56 +0700

    Add spesialis

--- src/app/tenaga-medis/dokter.ts
+++ src/app/tenaga-medis/dokter.ts
@@ -3,6 +3,7 @@ import { TenagaMedis }		from './tenaga-medis';
 export class Dokter {
 	constructor (
 		public no_pegawai: string = null,
+		public spesialis: string = null,
 		public tenaga_medis: TenagaMedis = new TenagaMedis()
 	) {	}
 }
