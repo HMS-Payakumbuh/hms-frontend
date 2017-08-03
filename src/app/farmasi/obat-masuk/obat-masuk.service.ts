28f38962991c0bd7fca09401461cb734143fa835
Hans Christian Gunawan <hcguyz@gmail.com>
2017-08-03 13:32:58 +0700

    Refactor pemakaian obat API to allow time range

--- src/app/farmasi/obat-masuk/obat-masuk.service.ts
+++ src/app/farmasi/obat-masuk/obat-masuk.service.ts
@@ -30,9 +30,22 @@ export class ObatMasukService {
 			.map(allObatMasuk => allObatMasuk.find(obat_masuk => obat_masuk.id == id));
 	}
 
-	getTodayObatMasuk(id_stok_obat: number): Observable<ObatMasuk[]> {
+	/* getTodayObatMasuk(id_stok_obat: number): Observable<ObatMasuk[]> {
 		return this.http.get(this.obatMasukUrl + '/today/' + id_stok_obat)
 			.map((res: Response) => res.json());
+	} */
+
+	getObatMasukByTime(waktu_mulai: Date, waktu_selesai: Date, id_stok_obat: number): Observable<ObatMasuk[]> {
+		let params: URLSearchParams = new URLSearchParams();
+		params.set('id_stok_obat', ''+id_stok_obat);
+		params.set('waktu_mulai', waktu_mulai.toLocaleString());
+		params.set('waktu_selesai', waktu_selesai.toLocaleString());
+
+		let requestOptions = new RequestOptions();
+		requestOptions.params = params;
+
+		return this.http.get(this.obatMasukUrl+'/search_by_time', requestOptions)
+		    .map((res: Response) => res.json());		
 	}
 
 	createObatMasuk(obatMasuk: ObatMasuk) {
--- src/app/farmasi/obat-tindakan/obat-tindakan.service.ts
+++ src/app/farmasi/obat-tindakan/obat-tindakan.service.ts
@@ -29,9 +29,22 @@ export class ObatTindakanService {
 			.map(allObatTindakan => allObatTindakan.find(obat_tindakan => obat_tindakan.id == id));
 	}
 
-	getTodayObatTindakan(id_stok_obat: number): Observable<ObatTindakan[]> {
+	/* getTodayObatTindakan(id_stok_obat: number): Observable<ObatTindakan[]> {
 		return this.http.get(this.obatTindakanUrl + '/today/' + id_stok_obat)
 			.map((res: Response) => res.json());
+	} */
+
+	getObatTindakanByTime(waktu_mulai: Date, waktu_selesai: Date, id_stok_obat: number): Observable<ObatTindakan[]> {
+		let params: URLSearchParams = new URLSearchParams();
+		params.set('id_stok_obat', ''+id_stok_obat);
+		params.set('waktu_mulai', waktu_mulai.toLocaleString());
+		params.set('waktu_selesai', waktu_selesai.toLocaleString());
+
+		let requestOptions = new RequestOptions();
+		requestOptions.params = params;
+
+		return this.http.get(this.obatTindakanUrl+'/search_by_time', requestOptions)
+		    .map((res: Response) => res.json());		
 	}
 
 	createObatTindakan(obatTindakan: ObatTindakan[]) {
